cfdb8e15c441a50d52aecaf9947a2fe4e472e676
Rololow <robinwarichet@gmail.com>
2022-11-12 00:30:23 +0100

    basic counter

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,32 @@
 use yew::prelude::*;
 
+struct Model {
+    value: i64
+}
+
 #[function_component(App)]
 fn app() -> Html {
+    let state = use_state(|| Model {
+        value: 0
+    });
+
+    let onclick = {
+        let state = state.clone();
+
+        Callback::from(move |_| {
+            state.set(Model {
+                value: state.value + 1
+            })
+        })
+    };
+
     html! {
-        <h1>{ "Hello World" }</h1>
+        <div>
+            <button {onclick}>
+                { "+1" }
+            </button>
+            <p>{ state.value }</p>
+        </div>
     }
 }
 
